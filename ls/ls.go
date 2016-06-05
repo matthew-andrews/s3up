217c42ba37ab524228ff42625203b99aaa551233
Matthew Andrews <matt@mattandre.ws>
2016-06-05 17:59:53 +0100

    Support strip option in the ls package

--- ls/ls.go
+++ ls/ls.go
@@ -5,15 +5,18 @@ import (
 	"mime"
 	"path"
 	"path/filepath"
+	"strings"
 )
 
 type File struct {
-	Filename    string
+	Location    string
+	Key         string
 	Etag        string
 	ContentType string
 }
 
-func GetFiles(directory string) ([]File, error) {
+func GetFiles(directory string, strip int) ([]File, error) {
+	directory = filepath.Clean(directory)
 	var output []File
 	files, err := ioutil.ReadDir(directory)
 	if err != nil {
@@ -22,18 +25,23 @@ func GetFiles(directory string) ([]File, error) {
 	for _, file := range files {
 		name := path.Join(directory, file.Name())
 		if file.IsDir() {
-			subFiles, err := GetFiles(name)
+			subFiles, err := GetFiles(name, strip)
 			if err != nil {
 				return output, err
 			}
 			output = append(output, subFiles...)
 		} else {
 			output = append(output, File{
-				Filename:    name,
+				Key:         StripFromName(name, strip),
+				Location:    name,
 				Etag:        "TODO",
 				ContentType: mime.TypeByExtension(filepath.Ext(name)),
 			})
 		}
 	}
 	return output, nil
+}
+
+func StripFromName(name string, strip int) string {
+	return strings.Join(strings.Split(name, "/")[strip:], "/")
 }
--- ls/ls_test.go
+++ ls/ls_test.go
@@ -7,14 +7,15 @@ import (
 )
 
 func TestGetFilesWithAFolderWithASingleFile(t *testing.T) {
-	files, err := GetFiles("../fixtures/one-file")
+	files, err := GetFiles("../fixtures/one-file", 3)
 	if err != nil {
 		t.Fatalf("Unexpected error, %v\n", err)
 	}
 
 	if err = fileSlicesAreEquivalent(files, []File{
 		File{
-			Filename:    "../fixtures/one-file/my-file.txt",
+			Key:         "my-file.txt",
+			Location:    "../fixtures/one-file/my-file.txt",
 			ContentType: "text/plain; charset=utf-8",
 			Etag:        "TODO",
 		},
@@ -24,19 +25,21 @@ func TestGetFilesWithAFolderWithASingleFile(t *testing.T) {
 }
 
 func TestGetFilesWithAFolderWithSubfolders(t *testing.T) {
-	files, err := GetFiles("../fixtures/subfolders")
+	files, err := GetFiles("../fixtures/subfolders", 3)
 	if err != nil {
 		t.Fatalf("Unexpected error, %v\n", err)
 	}
 
 	if err = fileSlicesAreEquivalent(files, []File{
 		File{
-			Filename:    "../fixtures/subfolders/subsubfolder/bottom-file.txt",
+			Key:         "subsubfolder/bottom-file.txt",
+			Location:    "../fixtures/subfolders/subsubfolder/bottom-file.txt",
 			ContentType: "text/plain; charset=utf-8",
 			Etag:        "TODO",
 		},
 		File{
-			Filename:    "../fixtures/subfolders/top-file.txt",
+			Key:         "top-file.txt",
+			Location:    "../fixtures/subfolders/top-file.txt",
 			ContentType: "text/plain; charset=utf-8",
 			Etag:        "TODO",
 		},
@@ -44,6 +47,20 @@ func TestGetFilesWithAFolderWithSubfolders(t *testing.T) {
 		t.Fatalf("Unexpected error, %v\n", err)
 	}
 
+}
+
+func TestStripFromName(t *testing.T) {
+	stripMap := map[int]string{
+		0: "../fixtures/one-file/my-file.txt",
+		1: "fixtures/one-file/my-file.txt",
+		2: "one-file/my-file.txt",
+		3: "my-file.txt",
+	}
+	for strip, expected := range stripMap {
+		if actual := StripFromName("../fixtures/one-file/my-file.txt", strip); actual != expected {
+			t.Fatalf("expected %s to equal %s", actual, expected)
+		}
+	}
 }
 
 func fileSlicesAreEquivalent(expected []File, actual []File) error {
