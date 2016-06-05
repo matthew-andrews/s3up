e445000c4b0c50d00f0458a178da70e447947873
Matthew Andrews <matt@mattandre.ws>
2016-06-05 21:08:23 +0100

    Injectable

--- s3client/s3client.go
+++ s3client/s3client.go
@@ -1,19 +1,21 @@
-package s3
+package s3client
 
 import (
 	"errors"
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/matthew-andrews/s3up/objects"
 	"os"
 )
 
-func Upload(bucket string, files []objects.File) error {
+type Client struct {
+	Service *s3.S3
+}
+
+func (client *Client) Upload(bucket string, files []objects.File) error {
 	if len(files) < 1 {
 		return errors.New("No files found for upload to S3.  (Directories are ignored)")
 	}
-	svc := s3.New(session.New())
 
 	for _, file := range files {
 		fmt.Printf("%s to %s\n", file.Location, file.Key)
@@ -22,7 +24,7 @@ func Upload(bucket string, files []objects.File) error {
 			return errors.New(fmt.Sprintf("Could not open file: %s", file.Location))
 		}
 		defer realFile.Close()
-		resp, err := svc.PutObject(&s3.PutObjectInput{
+		resp, err := client.Service.PutObject(&s3.PutObjectInput{
 			Body:         realFile,
 			Bucket:       &bucket,
 			Key:          &file.Key,
