f7fb5b66f47c1e03e5338941ae9110524ec6afcf
Matthew Andrews <matt@mattandre.ws>
2016-06-10 22:07:25 +0100

    Fix version

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "S3 Upload"
 	app.Usage = "Uploads files to S3"
-	app.Version = "v1.0.0"
+	app.Version = "v1.0.1"
 	app.Flags = []cli.Flag{
 		cli.IntFlag{
 			Name:  "strip",
