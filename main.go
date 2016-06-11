195beacd7547d8b2d536e89da8d1e2c479d44d24
Matthew Andrews <matt@mattandre.ws>
2016-06-11 10:46:31 +0100

    Bump version

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "S3 Upload"
 	app.Usage = "Uploads files to S3"
-	app.Version = "v1.0.3"
+	app.Version = "v1.0.4"
 	app.Flags = []cli.Flag{
 		cli.IntFlag{
 			Name:  "strip",
